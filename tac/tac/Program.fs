c63e8f896ed17da120a97eb1dc5d795ceed3bf35
Jack Wolverson <jwwolverson@gmail.com>
2017-04-27 12:27:11 +0100

    Add files via upload

---
+++ tac/tac/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+ 
+open System 
+open System.IO
+
+let doesExist path = 
+    match File.Exists(path) with
+    | true -> printfn "Valid file path "
+    | false -> printfn "Please Insert a Valid File Path "
+
+let rec reverse file =
+    match file with 
+    |x::xs -> reverse xs ; printfn "%s" x 
+    |_ -> printf ""
+
+let readfile path = 
+    let openfile = File.OpenRead(path)
+    let file = (Array.toList (File.ReadAllLines (path)))
+    reverse file 
+
+
+[<EntryPoint>]
+    let main args = 
+        doesExist args.[0]
+        Array.iter readfile args
+        printfn "\n"
+        0 // return an integer exit code
+
